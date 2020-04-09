f3da866ce6c38fd509958c7fd49af75dc326a8eb
karatugo <karatugo@gmail.com>
2020-04-09 14:48:44 +0300

    Add project level permissions

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -3,11 +3,9 @@ from .models import Post
 from .serializers import PostSerializer
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
